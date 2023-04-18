bd25b7ca2e964055ed55407a5aff4cd897116b15
Janice-emefa <81053159+Janice-emefa@users.noreply.github.com>
2023-04-18 15:01:23 +0000

    Update code.py

--- code.py
+++ code.py
@@ -6,7 +6,7 @@ file_name = r"C:\Users\Acity\Videos\classwork\quotes.json"  # Update this with t
 def read_quotes_from_json(file_name):
     """Reads quotes from a JSON file and returns a list of dictionaries containing the quote and its author."""
     with open(file_name, "r", encoding="utf-8") as f:
-        data = json.load(f)
+        data = json.load(f)   
         quotes = data["quotes"]
     return quotes
 def get_random_quote(quotes):
@@ -14,7 +14,7 @@ def get_random_quote(quotes):
     return random.choice(quotes)
 
 def add_new_quote(quotes, quote, author):
-    """Adds a new quote and its author to the list of quotes."""
+    """Adds a new quote and its author to the list of quotes already available."""
     new_quote = {"quote": quote, "author": author}
     quotes.append(new_quote)
 
@@ -29,15 +29,15 @@ def main():
     file_name = "quotes.json"  # Update this with the name of your JSON file
     quotes = read_quotes_from_json(file_name)
     while True:
-        print("1. Generate a random quote")
-        print("2. Input a new quote")
-        print("3. Exit")
+        print("1. Generate a random quote")  #Generates a random quote from the list
+        print("2. Input a new quote")        #Enter or input a new quote
+        print("3. Exit")                     
         choice = input("Enter your choice (1/2/3): ")
         if choice == "1":
             if quotes:
                 quote = get_random_quote(quotes)
                 print("Quote: ", quote["quote"])
-                print("Author: ", quote["author"])
+                print("Author: ", quote["author"]) 
             else:
                 print("No quotes found in the file.")
         elif choice == "2":
